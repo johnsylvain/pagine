65de7373069616e1f3fb3afec9b61c29c8e18fbf
John Sylvain <john.n.sylvain@gmail.com>
2017-10-03 16:47:05 -0500

    configured webpack and build process

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var env = process.env.WEBPACK_ENV;
+
+var appName = 'pagine';
+var plugins = [], outputFile;
+
+if (env === 'build') {
+  plugins.push(new UglifyJsPlugin({ minimize: true }));
+  outputFile = appName + '.min.js';
+} else {
+  outputFile = appName + '.js';
+}
+
+var config = {
+  entry: __dirname + '/src/index.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: 'Pagine',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  plugins: plugins
+};
+
+module.exports = config;
