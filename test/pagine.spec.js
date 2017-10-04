d45a3ef8c0452f17e9f3ddbf34a13084f481034b
John Sylvain <john.n.sylvain@gmail.com>
2017-10-03 19:41:06 -0500

    created tests for markdown and routes

--- test/pagine.spec.js
+++ test/pagine.spec.js
@@ -1,9 +1,40 @@
-describe('Jest test', () => {
-    it('should be true', () => {
-        expect(true).toBeTruthy();
-    })
+import Pagine from '../src/index';
 
-    // TODO: add tests for pagine
-    // - transform markdown
-    // - create dynamic routing
+function bootstrap() {
+    beforeEach(() => {
+        this.pagine = new Pagine({
+            routes: [
+                {
+                    path: '/',
+                    md: '## home page'                    
+                },
+                {
+                    path: '/about',
+                    md: '## about page'
+                }
+            ]
+        });
+    });
+}
+
+describe('Pagine', function() {
+    
+
+    describe('routing', function() {
+        bootstrap.call(this, undefined);
+
+        it('should create routes', () => {
+            expect(this.pagine.router._routes.length).toBe(2);
+        });
+    });
+
+    describe('markdown transformation', function() {
+        bootstrap.call(this, undefined);
+        
+        it('should compile markdown to html', () => {
+            const md = this.pagine.compileMarkdown('# hello');
+            expect(md.html).toEqual('<h1 id=\"hello\">hello</h1>')
+        })
+    });
+
 });
