03b2b71dab175f3eba16b97cb441cc6cf791860a
John Sylvain <john.n.sylvain@gmail.com>
2017-10-04 09:42:13 -0500

    replaced mdify with showdown and updated tests

--- src/index.js
+++ src/index.js
@@ -1,32 +1,33 @@
-import mdify from 'mdify';
+import { Converter } from 'showdown';
 import Navigo from 'navigo';
 import template from 'lodash/template'
 
 export default class Pagine {
-    constructor(settings) {
-        this.router = new Navigo(null, true);
-        this.template = template(document.body.textContent.trim());
+  constructor(settings) {
+    this.router = new Navigo(null, true);
+    this.template = template(document.body.textContent.trim());
+    this.converter = new Converter();
 
-        this.createRoutes(settings.routes);
-    }
-
-    setContent(md) {
-        this.template({
-            content: this.compileMarkdown(md)
-        })
-    }
-
-    createRoutes(routes) {
-        this.router.on(routes.reduce((cur, acc) => {
-            return Object.assign({}, acc, {
-                [cur.path]: function() {
-                    this.setContent(cur.md);
-                }
-            })
-        }, {})).resolve()
-    }
-
-    compileMarkdown(md) {
-        return mdify.parse(md);
-    }
+    this.createRoutes(settings.routes);
+  }
+  
+  createRoutes(routes) {
+    this.router.on(routes.reduce((cur, acc) => {
+      return Object.assign({}, acc, {
+        [cur.path]:  () => {
+          this.setContent(cur.md);
+        }
+      })
+    }, {})).resolve()
+  }
+  
+  setContent(md) {
+    this.template({
+      content: this.compileMarkdown(md)
+    })
+  }
+  
+  compileMarkdown(md) {
+    return this.converter.makeHtml(md);
+  }
 }
--- test/pagine.spec.js
+++ test/pagine.spec.js
@@ -1,40 +1,40 @@
 import Pagine from '../src/index';
 
 function bootstrap() {
-    beforeEach(() => {
-        this.pagine = new Pagine({
-            routes: [
-                {
-                    path: '/',
-                    md: '## home page'                    
-                },
-                {
-                    path: '/about',
-                    md: '## about page'
-                }
-            ]
-        });
+  beforeEach(() => {
+    this.pagine = new Pagine({
+      routes: [
+        {
+          path: '/',
+          md: '## home page'                    
+        },
+        {
+          path: '/about',
+          md: '## about page'
+        }
+      ]
     });
+  });
 }
 
 describe('Pagine', function() {
     
 
-    describe('routing', function() {
-        bootstrap.call(this, undefined);
+  describe('routing', function() {
+    bootstrap.call(this, undefined);
 
-        it('should create routes', () => {
-            expect(this.pagine.router._routes.length).toBe(2);
-        });
+    it('should create routes', () => {
+      expect(this.pagine.router._routes.length).toBe(2);
     });
+  });
 
-    describe('markdown transformation', function() {
-        bootstrap.call(this, undefined);
-        
-        it('should compile markdown to html', () => {
-            const md = this.pagine.compileMarkdown('# hello');
-            expect(md.html).toEqual('<h1 id=\"hello\">hello</h1>')
-        })
-    });
+  describe('markdown transformation', function() {
+    bootstrap.call(this, undefined);
+    
+    it('should compile markdown to html', () => {
+      const md = this.pagine.compileMarkdown('# hello');
+      expect(md).toEqual('<h1 id=\"hello\">hello</h1>')
+    })
+  });
 
 });
