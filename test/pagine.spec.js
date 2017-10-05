92381f07ee9b03d3843555a9f36aa64f4baf9ac7
John Sylvain <john.n.sylvain@gmail.com>
2017-10-05 17:52:22 -0500

    added rendering of template into view

--- test/pagine.spec.js
+++ test/pagine.spec.js
@@ -1,34 +1,37 @@
 import Pagine from '../lib/pagine.js';
+import { $ } from '../lib/dom';
 
 function bootstrap() {
   beforeEach(() => {
     this.pagine = new Pagine({
+      view: '#view',
       routes: [
         {
           path: '/home',
           md: '## home page',
-          layout: 'main'
+          layout: '#main'
         },
         {
           path: '/about',
           md: '## about page',
-          layout: 'main'
+          layout: '#main'
         }
       ]
     });
+    document.body.innerHTML = `
+      <div id="view"></div>
+      <script type="text/html" id="main">
+        <%= content %>
+      </script>
+    `
   });
 
-  document.body.innerHTML = `
-    <div id="main">
-      <%= content %>
-    </div>
-  `
 }
 
-describe('Pagine', function() {
+describe('Pagine module', function() {
 
 
-  describe('routing', function() {
+  describe('router', function() {
     bootstrap.call(this);
 
     it('should create routes', () => {
@@ -44,5 +47,32 @@ describe('Pagine', function() {
       expect(md).toEqual('<h1>hello</h1>')
     })
   });
+
+  describe('view rendering', () => {
+    bootstrap.call(this);
+
+    it('should compile template into view', () => {
+      this.pagine.setContent('main', '# Readme');
+
+      var output = $('#view').first().innerHTML.trim()
+      console.log(output);
+      expect(output).toBe('<h1>Readme</h1>');
+    });
+
+    /*
+     * TODO: Fix this unit test to change URL location
+     */
+    it('should change view content on route change', () => {
+      var output;
+
+      window.location.url = '#/home';
+      output = $('#view').first().innerHTML.trim()
+      expect(output).toBe('<h2>home page</h2>');
+
+      window.location.url = '#/about';
+      output = $('#view').first().innerHTML.trim()
+      expect(output).toBe('<h2>about page</h2>')
+    });
+  })
 
 });
