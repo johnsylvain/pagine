ec50454b8452d449b1b764fddc9dfe94416d3da2
John Sylvain <john.n.sylvain@gmail.com>
2017-10-05 14:43:05 -0500

    fix (#4): created custom templating engine

---
+++ test/templateEngine.spec.js
@@ -0,0 +1,22 @@
+import Template from '../lib/templateEngine';
+
+describe('Template Engine', function() {
+  beforeEach(() => {
+    this.template = new Template();
+
+    document.body.innerHTML = `
+      <script type="text/html" id="main">
+        <h1><%= content %></h1>
+      </script>
+    `
+  });
+
+  it('should create a new engine', () => {
+    expect(this.template).toBeInstanceOf(Template);
+  });
+
+  it('should inject data into template', () => {
+    var htmlStr = this.template.tmpl('main', {content: 'hello'}).trim();
+    expect(htmlStr).toBe('<h1>hello</h1>');
+  })
+})
