d903b7db937a642f8a5f0ef20497dcb06d68fc1a
John Sylvain <john.n.sylvain@gmail.com>
2018-01-13 08:48:37 -0600

    improve assign util function

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
-export const assign = (obj, ...props) => {
-  props.forEach(prop => {
-    for (let i in prop) obj[i] = prop[i]
-  })
-	return obj
-}
+// Object.assign fill-in
+export const assign = (obj, ...props) => props.reduce((acc, prop) => {
+  for (let i in prop) acc[i] = prop[i]
+  return acc
+}, obj)
 
+// compose functions left-to-right
 export const compose = (...fns) => (initialValue) =>
   fns.reduce((val, fn) => fn(val), initialValue)
